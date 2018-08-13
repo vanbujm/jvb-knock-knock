8f75b5aeef65d7bd596412ff0f9185140ea83bea
Jonathan Van Buren <vanbujm@gmail.com>
2018-08-13 22:37:46 +0800

    Removed middleware and cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import http from 'http';
 import express from 'express';
-import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import middleware from './middleware';
 import api from './api';
 import config from './config.json';
 
@@ -13,13 +11,8 @@ app.server = http.createServer(app);
 // logger
 app.use(morgan('dev'));
 
-// 3rd party middleware
-app.use(cors({
-	exposedHeaders: config.corsHeaders
-}));
-
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+  limit: config.bodyLimit
 }));
 
 // internal middleware
