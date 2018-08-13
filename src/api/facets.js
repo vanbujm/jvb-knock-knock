669c6e81002c80ee1453c4b14a4f7fccc0f610b9
Jonathan Van Buren <vanbujm@gmail.com>
2018-08-13 19:41:20 +0800

    Removed db

--- src/api/facets.js
+++ src/api/facets.js
@@ -1,7 +1,7 @@
 import resource from 'resource-router-middleware';
 import facets from '../models/facets';
 
-export default ({ config, db }) => resource({
+export default ({ config }) => resource({
 
 	/** Property name to store preloaded entity on `request`. */
 	id : 'facet',
